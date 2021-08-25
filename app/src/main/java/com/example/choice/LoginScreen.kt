670cb7bdb2d5fbeca3bba52a1d568a891c15b089
DanielK2001 <daniel.kathiresan@gmail.com>
2021-08-25 18:44:26 +1200

    Revert "Revert "Revert "Adding a login and registration screen / fragment. Adding slight styling to rest of program (removing menu bar etc) Adding assests (logo, background) as placeholders for future assests. Slight changes in gradle build files to make file run better. Importing google assets. Changing default screeen in android manifest"""
    This reverts commit d7ac872cc5d5872d86c368ae322d0e5452c2e305.

--- app/src/main/java/com/example/choice/LoginScreen.kt
+++ app/src/main/java/com/example/choice/LoginScreen.kt
@@ -2,28 +2,10 @@ package com.example.choice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 
-class LoginScreen : AppCompatActivity(), FragmentNavigation{
+class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container,LoginFragment())
-            .commit()
     }
-
-    override fun navigateFrag(fragment: Fragment, addToStack: Boolean) {
-        val transaction = supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container,fragment)
-
-        if(addToStack){
-            transaction.addToBackStack(null)
-        }
-
-        transaction.commit()
-    }
-
 }
