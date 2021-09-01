357bf88c3b75e02bfc887021cc636658dee736a9
DanielK2001 <daniel.kathiresan@gmail.com>
2021-09-01 17:03:29 +1200

    Added error checking to the login and the registration screen. Check if form is empty or if field is entered correctly. Added message on successfull login / register

--- app/src/main/java/com/example/choice/LoginFragment.kt
+++ app/src/main/java/com/example/choice/LoginFragment.kt
@@ -1,16 +1,23 @@
 package com.example.choice
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 
 /**
  * A simple [Fragment] subclass.
  */
 class LoginFragment : Fragment() {
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,10 +26,57 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_login, container, false)
 
+        username = view.findViewById(R.id.login_username)
+        password = view.findViewById(R.id.login_password)
+
         view.findViewById<Button>(R.id.registerButton).setOnClickListener {
             var navRegister = activity as FragmentNavigation
             navRegister.navigateFrag(RegisterFragment(), false)
         }
+
+        view.findViewById<Button>(R.id.loginButton).setOnClickListener {
+            //Run validation function when called
+            validateForm()
+
+        }
+
         return view
+    }
+
+
+    private fun validateForm(){
+        //Show warning icon (currently placeholder) when a criteria is met
+        val icon = AppCompatResources.getDrawable(requireContext(),
+            R.drawable.warningph)
+
+        icon?.setBounds(0, 0,icon.intrinsicWidth,icon.intrinsicHeight)
+        //check string user has entered
+        when{
+            TextUtils.isEmpty((username.text.toString().trim()))->{
+
+                username.setError("Please Enter Username",icon)
+            }
+            TextUtils.isEmpty((password.text.toString().trim()))->{
+
+                password.setError("Please Enter Password",icon)
+            }
+
+            username.text.toString().isNotEmpty() &&
+                    password.text.toString().isNotEmpty() ->
+            {//Currently checking if username is equal to email,will change to email later
+                if (username.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))){
+                    //Correct if matches format, then check password
+                    if(password.text.toString().length>=5){
+                        //If more than 5 characters check confirm password
+                        Toast.makeText(context,"Login successful", Toast.LENGTH_SHORT).show()
+                    }else{
+                        password.setError("Please enter at least 5 characters",icon)
+                    }
+                }else{
+                    username.setError("Please Enter Valid Email Id",icon)
+                }
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/choice/RegisterFragment.kt
+++ app/src/main/java/com/example/choice/RegisterFragment.kt
@@ -1,16 +1,24 @@
 package com.example.choice
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 
 /**
  * A simple [Fragment] subclass.
  */
 class RegisterFragment : Fragment() {
+    //Init variables
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var cnfrmPassword: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,12 +26,71 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_register, container, false)
+        //Variables for user info
+        username = view.findViewById(R.id.reg_username)
+        password = view.findViewById(R.id.reg_password)
+        cnfrmPassword = view.findViewById(R.id.reg_cnfrm_password)
 
         view.findViewById<Button>(R.id.return_login_btn).setOnClickListener {
             var navRegister = activity as FragmentNavigation
             navRegister.navigateFrag(LoginFragment(), false)
         }
+        //password validation and error checking
+        view.findViewById<Button>(R.id.create_account_btn).setOnClickListener {
+            //Run validation function when called
+            validateEmptyForm()
+
+        }
+
         return view
 
+    }
+
+    private fun validateEmptyForm(){
+        //Show warning icon (currently placeholder) when a criteria is met
+        val icon = AppCompatResources.getDrawable(requireContext(),
+        R.drawable.warningph)
+
+        icon?.setBounds(0, 0,icon.intrinsicWidth,icon.intrinsicHeight)
+        //check string user has entered
+        when{
+            TextUtils.isEmpty((username.text.toString().trim()))->{
+
+                username.setError("Please Enter Username",icon)
+            }
+            TextUtils.isEmpty((password.text.toString().trim()))->{
+
+                password.setError("Please Enter Password",icon)
+            }
+            TextUtils.isEmpty((cnfrmPassword.text.toString().trim()))->{
+
+                cnfrmPassword.setError("Please Confirm Password",icon)
+            }
+
+            username.text.toString().isNotEmpty() &&
+                    password.text.toString().isNotEmpty()&&
+                    cnfrmPassword.text.toString().isNotEmpty() ->
+            {//Currently checking if username is equal to email,will change to email later
+                if (username.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))){
+                    //Correct if matches format, then check password
+                    if(password.text.toString().length>=5){
+                        //If more than 5 characters check confirm password
+                        if(password.text.toString() == cnfrmPassword.text.toString()){
+
+                            Toast.makeText(context,"Account Creation Successful",Toast.LENGTH_SHORT).show()
+                        }
+                        else{
+                            cnfrmPassword.setError("Password Does Not Match",icon)
+                        }
+                    }else{
+                        password.setError("Please enter at least 5 characters",icon)
+                    }
+                }else{
+                    username.setError("Please Enter Valid Email Id",icon)
+                }
+
+
+            }
+        }
     }
 }
