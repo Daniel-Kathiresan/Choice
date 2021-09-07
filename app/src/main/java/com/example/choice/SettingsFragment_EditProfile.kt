32f7ed50f1c8aac8b62ed9a6eb8bccfc0e53d0e7
Jason-5-37 <83677748+Jason-5-37@users.noreply.github.com>
2021-09-07 19:32:55 +1200

    Merge branch 'Daniel' of https://github.com/Daniel-Kathiresan/Choice into Jason
     Conflicts:
    	.idea/deploymentTargetDropDown.xml
    	.idea/misc.xml
    	app/src/main/AndroidManifest.xml
    	app/src/main/res/values/strings.xml
    
    Working on the firebase

---
+++ app/src/main/java/com/example/choice/SettingsFragment_EditProfile.kt
@@ -0,0 +1,43 @@
+package com.example.choice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.choice.databinding.ActivitySettingsFragmentEditProfileBinding
+import kotlinx.android.synthetic.main.activity_settings_fragment_edit_profile.*
+
+class SettingsFragment_EditProfile : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsFragmentEditProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_fragment_edit_profile)
+        binding = ActivitySettingsFragmentEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val getImage = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                binding.editProfileChangeheadButton.set
+            }
+        )
+
+
+        edit_profile_done_text_view.setOnClickListener {
+            val intent = Intent(this, SettingsScreen::class.java)
+            startActivity(intent)
+        }
+
+        edit_profile_cancel_text_view.setOnClickListener {
+            val intent = Intent(this, SettingsScreen::class.java)
+            startActivity(intent)
+        }
+
+        edit_profile_changehead_button.setOnClickListener {
+            getImage.launch("image/*")
+        }
+    }
+}
