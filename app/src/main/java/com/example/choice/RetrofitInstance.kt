7652d1390c23c042ecc6ae6deea5e2e19bc95245
Scott Yang <scogranger@gmail.com>
2021-09-15 18:01:00 +1200

    friend list and chat user interface, and notification things

---
+++ app/src/main/java/com/example/choice/RetrofitInstance.kt
@@ -0,0 +1,25 @@
+package com.example.choice
+
+
+import com.example.choice.`interface`.NotificationApi
+import com.example.choice.constants.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class RetrofitInstance {
+
+    companion object{
+        private val retrofit by lazy{
+            Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        val api by lazy{
+            retrofit.create(NotificationApi::class.java)
+        }
+
+    }
+}
