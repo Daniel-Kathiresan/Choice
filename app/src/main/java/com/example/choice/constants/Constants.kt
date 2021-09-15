08bde62829cc7924b0ac3bf3d94ed448a539979b
Scott Yang <scogranger@gmail.com>
2021-09-15 18:08:13 +1200

    change server key

--- app/src/main/java/com/example/choice/constants/Constants.kt
+++ app/src/main/java/com/example/choice/constants/Constants.kt
@@ -3,7 +3,7 @@ package com.example.choice.constants
 class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAGmIMVe8:APA91bFjkbiK9axKqTFC4AWbySTxdcKOq4hZuhoBoF6nFSUTo1c3sRvFjXRrdV1YsuUyMrMoTM9LbYz0-q0vZRfGndYFGuWxVincKoMsSR5qACsEXvWqkMFSkVW96Q54I2FbuGqb-Ccf"
+        const val SERVER_KEY = "Key here"
         const val CONTENT_TYPE = "application/json"
     }
 }
