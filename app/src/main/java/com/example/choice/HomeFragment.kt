b3bb0ad881df3b4aba56742393eab3fac14e39b1
DanielK2001 <daniel.kathiresan@gmail.com>
2021-09-17 21:11:08 +1200

    Added information display

--- app/src/main/java/com/example/choice/HomeFragment.kt
+++ app/src/main/java/com/example/choice/HomeFragment.kt
@@ -7,17 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
-
-/**
- * A simple [Fragment] subclass.
- */
+import kotlinx.android.synthetic.main.fragment_home.*
+import org.w3c.dom.Text
 
 class HomeFragment : Fragment() {
+
+    private lateinit var fAuth: FirebaseAuth
+    private var firebaseUserID : String = ""
+    private lateinit var database: DatabaseReference
 
   override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,13 +31,38 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_home, container, false)
         //Adds logout functionality to app
-
+        fAuth = Firebase.auth
+        firebaseUserID = fAuth.currentUser!!.uid
       view.findViewById<Button>(R.id.log_out_btn).setOnClickListener(){
             Firebase.auth.signOut()
             var navLogin = activity as FragmentNavigation
             navLogin.navigateFrag(LoginFragment(),addToStack = false)
         }
+
+      if (firebaseUserID != null){
+          database = FirebaseDatabase.getInstance().getReference("Users")
+          database.child(firebaseUserID).get().addOnSuccessListener {
+              if (it.exists()){
+                  val fname = it.child("first name").value
+                  val lname = it.child("last name").value
+                  val gender = it.child("gender").value
+                  view.findViewById<TextView>(R.id.fullnamefield).text = (fname.toString() + " " + lname.toString())
+                  view.findViewById<TextView>(R.id.genderfield).text= (gender.toString())
+              }else{
+              }
+          }
+        }else {
+
+
+      }
+
+
+
       return view
   }
+
+
+
+
 
 }
--- app/src/main/java/com/example/choice/SettingsFragment.kt
+++ app/src/main/java/com/example/choice/SettingsFragment.kt
@@ -1,23 +1,79 @@
 package com.example.choice
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_settings_fragment.*
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_settings_screen.*
+import java.util.*
+
+// import com.google.firebase.auth.ktx.actionCodeSettings
 
 class SettingsFragment : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings_fragment)
+        setContentView(R.layout.activity_settings_screen)
 
-        setting_fragment_cancel_text_view.setOnClickListener {
-            val intent = Intent(this, SettingsScreen::class.java)
+
+
+        setting_button.setOnClickListener {
+            val intent = Intent(this, SettingsFragment::class.java)
             startActivity(intent)
         }
 
-        setting_fragment_done_text_view.setOnClickListener {
-            val intent = Intent(this, SettingsScreen::class.java)
-            startActivity(intent)
+        setting_user_head_image.setOnClickListener {
+            Log.d("Setting Activity", "Try to select photo")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type= "image/*"
+            startActivityForResult(intent,0)
+            uploadPhotoToFirebaseStorage()
+
         }
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            Log.d("Setting Activity", "Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            setting_user_head_image.setBackgroundDrawable(bitmapDrawable)
+        }
+    }
+
+
+
+    private fun uploadPhotoToFirebaseStorage() {
+        if (selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                Log.d("Setting Activity", "Successfully uploaded image: ${it.metadata?.path}")
+            }
     }
 }
