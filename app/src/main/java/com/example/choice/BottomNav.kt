0c6e7ba9a2e27be48cc7a02fe2d2d1008e6dbb28
Scott Yang <scogranger@gmail.com>
2021-10-06 18:35:36 +1300

    New Bottom Nav Bar and completed

--- app/src/main/java/com/example/choice/BottomNav.kt
+++ app/src/main/java/com/example/choice/BottomNav.kt
@@ -2,10 +2,38 @@ package com.example.choice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.ContentView
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_bottom_nav.*
 
 class BottomNav : AppCompatActivity() {
+
+    private var fragment: Fragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_nav)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.dashboardContainer, ChatsFragment()).commit()
+            bottomChip.setItemSelected(R.id.btnChats)
+        }
+
+        bottomChip.setOnItemSelectedListener { id ->
+            when (id){
+                R.id.btnChats -> {
+                    fragment = ChatsFragment()
+                }
+                R.id.btnFriends -> {
+                    fragment = FriendsFragment()
+                }
+            }
+
+            fragment!!.let {
+                supportFragmentManager.beginTransaction().replace(R.id.dashboardContainer, fragment!!).commit()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/choice/MainActivity.kt
+++ app/src/main/java/com/example/choice/MainActivity.kt
@@ -10,7 +10,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_bottom_navigation_bar.*
 
 //Following code may be moved
 
