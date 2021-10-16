25ece3e18a0bbf06b05e6c41698bda130f28d6f4
Jason-5-37 <xrf4650@aut.ac.nz>
2021-10-16 18:38:45 +1300

    'clean'

--- app/src/main/java/com/example/choice/constants/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.choice.constants
-
-class Constants {
-    companion object{
-        const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAGmIMVe8:APA91bFjkbiK9axKqTFC4AWbySTxdcKOq4hZuhoBoF6nFSUTo1c3sRvFjXRrdV1YsuUyMrMoTM9LbYz0-q0vZRfGndYFGuWxVincKoMsSR5qACsEXvWqkMFSkVW96Q54I2FbuGqb-Ccf"
-        const val CONTENT_TYPE = "application/json"
-    }
-}
--- app/src/main/java/com/example/choice/model/Chat.kt
+++
@@ -1,3 +0,0 @@
-package com.example.choice.model
-
-data class Chat (var senderId:String = "", var receiverId:String = "", var message:String = "")
