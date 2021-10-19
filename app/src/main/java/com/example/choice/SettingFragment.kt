6d13aa3218e6d3dfafce0e6a770698b519832700
DanielK2001 <daniel.kathiresan@gmail.com>
2021-10-19 18:25:51 +1300

    Fixed showing image in settings screen and uploading to profile

--- app/src/main/java/com/example/choice/SettingFragment.kt
+++ app/src/main/java/com/example/choice/SettingFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
@@ -49,10 +50,13 @@ class SettingFragment : Fragment() {
             Log.d("Setting Fragment", "Go to Logout Fragment")
             val intent = Intent(activity, LogoutActivity::class.java)
             startActivity(intent)
+
         }
 
+
+
         //button to update the bio
-        view.findViewById<Button>(R.id.setting_updatee_button).setOnClickListener(){
+        view.findViewById<Button>(R.id.setting_updatee_button).setOnClickListener{
 
             val settingbioupdate = biofield.text.toString()
             val settingFirstName = FirstNamefield.text.toString()
@@ -93,16 +97,20 @@ class SettingFragment : Fragment() {
         if (firebaseUserID != null){
             database.child(firebaseUserID).get().addOnSuccessListener {
                 if (it.exists()){
-                    val fname = it.child("first name").value
-                    val lname = it.child("last name").value
                     val gender = it.child("gender").value
                     val bio = it.child("bio").value
                     val firstname = it.child("first_name").value
                     val lastname = it.child("last_name").value
+                    val profilepicture = it.child("profile_picture").value.toString()
+                    //Daniel Kathiresan Glide Rewrite
                     view.findViewById<TextView>(R.id.SettingGender).text= (gender.toString())
                     biofield.setText((bio.toString()))
                     FirstNamefield.setText((firstname.toString()))
                     LastNamefield.setText((lastname.toString()))
+                    //Daniel Kathiresan Glide Rewrite
+                    Glide.with(Userimage)
+                        .load(profilepicture)
+                        .into(Userimage)
 
                 }else{
                 }
@@ -121,11 +129,31 @@ class SettingFragment : Fragment() {
         val filename = UUID.randomUUID().toString()
         //set reference
         val ref = FirebaseStorage.getInstance().getReference("/profile/Userimage/$filename"+ " UID: " + fAuth.currentUser?.uid)
+        var pRef = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUserID)
+        val imageUpdateMap = HashMap<String, Any>()
 
+        //Daniel Kathiresan
+        //Set link to profile to Image
+        //Updated to get download url
         ref.putFile(selectedPhotoUri!!)
-            .addOnSuccessListener {
+            .addOnSuccessListener { it ->
                 Log.d("Setting Fragment", "Successfully uploaded image: ${it.metadata?.path}")
+                val result = it.metadata!!.reference!!.downloadUrl
+                result.addOnSuccessListener {
+                    val picUrl = it.toString()
+                    imageUpdateMap["profile_picture"] = picUrl
+                    pRef.updateChildren(imageUpdateMap).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(context, "Updated Image", Toast.LENGTH_SHORT)
+                        } else {
+                            Toast.makeText(context, "Unable to update image", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }
             }
+
+
     }
 
     //onActivityResult is for show the photo in the imageview and same as the startActivityForResult, google is not suggest to use but still work
@@ -136,8 +164,13 @@ class SettingFragment : Fragment() {
             Log.d("Setting Activity", "Photo was selected")
 
             selectedPhotoUri = data.data
+            println("Image link: $data.data.toString()")
 
-            Userimage.setImageURI(selectedPhotoUri)
+            //Daniel Kathiresan Glide Rewrite
+            //Userimage.setImageURI(selectedPhotoUri)
+            Glide.with(Userimage)
+                .load(selectedPhotoUri)
+                .into(Userimage)
         }
     }
 }
